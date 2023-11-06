8b9514dd75398b2c39d13df008230faf7252b8d4
JB <jasbasn@gmail.com>
2023-11-05 22:20:49 -0500

    Make green snake

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ WindowHeight = 480
 
 screen = pygame.display.set_mode((WindowWidth, WindowHeight))
 
-player = pygame.Rect((300,250,50,50))
+snake = pygame.Rect((300,250,50,50))
 
 clock = pygame.time.Clock()
 
@@ -19,22 +19,22 @@ while playing:
 
     screen.fill((0, 0, 0))
 
-    pygame.draw.rect(screen, (255,0,0), player)
+    pygame.draw.rect(screen, (0,255,0), snake)
 
     key = pygame.key.get_pressed()
 
     if key[pygame.K_a] == True or key[pygame.K_LEFT] == True:
-        if player.left >= moveRate:
-            player.move_ip(-moveRate, 0)
+        if snake.left >= moveRate:
+            snake.move_ip(-moveRate, 0)
     elif key[pygame.K_d] == True or key[pygame.K_RIGHT] == True:
-        if player.right <= WindowWidth - moveRate:
-            player.move_ip( moveRate, 0)
+        if snake.right <= WindowWidth - moveRate:
+            snake.move_ip( moveRate, 0)
     elif key[pygame.K_w] == True or key[pygame.K_UP] == True:
-        if player.top >= moveRate:
-            player.move_ip( 0, -moveRate)
+        if snake.top >= moveRate:
+            snake.move_ip( 0, -moveRate)
     elif key[pygame.K_s] == True or key[pygame.K_DOWN] == True:
-        if player.bottom <= WindowHeight - moveRate:
-            player.move_ip( 0, moveRate)
+        if snake.bottom <= WindowHeight - moveRate:
+            snake.move_ip( 0, moveRate)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
