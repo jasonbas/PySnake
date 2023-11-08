abd06e382c227d02976b0e0be77aec1ab27c933b
JB <jasbasn@gmail.com>
2023-11-07 21:32:52 -0500

    Make snake movement smoother while enforcing the snake body to be within a specific lane

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ class Direction(Enum):
 
 class Mouse:
     def __init__( self ):
-        print( random.randrange( 0, WindowWidth, 1) )
         self.body = pygame.Rect((random.randrange( 0, WindowWidth - BodySize, BodySize), 
                                  random.randrange( 0, WindowHeight - BodySize, BodySize), 
                                  BodySize, 
@@ -20,21 +19,49 @@ class Mouse:
 
 class Snake:
     def __init__( self ):
-        self.direction = Direction.UNDEFINED
         self.body = pygame.Rect(( WindowWidth / 2, WindowHeight / 2, 
-                                 BodySize, BodySize ))
+                                 BodySize, BodySize ))        
+        self.direction = Direction.UNDEFINED
+        self.requestedDirection = Direction.UNDEFINED
+        self.changingDirection = False
+        
+    def ChangeDirection( self, direction ):
+        if self.direction is direction:
+            return
+        
+        if self.direction is Direction.UNDEFINED:
+            self.direction = direction
+            return
+        
+        if self.direction is Direction.DOWN and direction is Direction.UP:
+            return
+        elif self.direction is Direction.UP and direction is Direction.DOWN:
+            return
+        elif self.direction is Direction.LEFT and direction is Direction.RIGHT:
+            return
+        elif self.direction is Direction.RIGHT and direction is Direction.LEFT:
+            return
+        
+        self.requestedDirection = direction
+        self.changingDirection = True
 
     def Move( self, moveRate ):
         if self.direction == Direction.UNDEFINED:
             return
-        
-        if self.direction == Direction.RIGHT or self.direction == Direction.LEFT:
-            remainder = self.body.top % BodySize
-            self.body.move_ip( 0, -remainder)
-        if self.direction == Direction.UP or self.direction == Direction.DOWN:
-            remainder = self.body.left % BodySize
-            self.body.move_ip( -remainder, 0)
 
+        if self.changingDirection is True:
+            if self.direction == Direction.RIGHT or self.direction == Direction.LEFT:
+                remainder = self.body.left % BodySize
+                if remainder is 0:
+                    self.direction = self.requestedDirection
+                    self.changingDirection = False          
+            if self.direction == Direction.UP or self.direction == Direction.DOWN:
+                remainder = self.body.top % BodySize
+                if remainder is 0:
+                    self.direction = self.requestedDirection
+                    self.changingDirection = False                  
+
+        #Move snake body
         if self.direction == Direction.LEFT:
             if self.body.left >= moveRate:
                 self.body.move_ip(-moveRate, 0)
@@ -54,7 +81,7 @@ pygame.init()
 WindowWidth = 640
 WindowHeight = 480
 BodySize = 20
-MoveRate = 2
+MoveRate = 4
 
 screen = pygame.display.set_mode((WindowWidth, WindowHeight))
 pygame.display.set_caption('Snake')
@@ -70,13 +97,13 @@ while playing:
     key = pygame.key.get_pressed()
 
     if key[pygame.K_a] == True or key[pygame.K_LEFT] == True:
-        snake.direction = Direction.LEFT
+        snake.ChangeDirection( Direction.LEFT )
     elif key[pygame.K_d] == True or key[pygame.K_RIGHT] == True:
-        snake.direction = Direction.RIGHT
+        snake.ChangeDirection( Direction.RIGHT )
     elif key[pygame.K_w] == True or key[pygame.K_UP] == True:
-        snake.direction = Direction.UP
+        snake.ChangeDirection( Direction.UP )
     elif key[pygame.K_s] == True or key[pygame.K_DOWN] == True:
-        snake.direction = Direction.DOWN
+        snake.ChangeDirection( Direction.DOWN )
 
     snake.Move( MoveRate )
 
